410be87b70e64ba996c92b3e23d236125f075e5f
iamshanipatel <shanipatel37780@gmail.com>
2026-02-26 21:34:34 +0530

    Add 13_few_2026 folder with Java file

---
+++ CheckNumber.java
@@ -0,0 +1,16 @@
+import java.util;
+class CkeckNumber
+{
+    public static voild main (Sting args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println('enter a number');
+        int x = sc.nextlnt();
+        if(x>0)
+        {
+            System.out.println("Negativ number")
+        }
+    }
+}
+
